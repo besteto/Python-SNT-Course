1cf2eadd1a914989c0459a0e1eb3b79fe1f87e32
Allien Po <besteto@gmail.com>
2014-04-26 16:11:17 +0400

    upd 1.1

--- 1_lesson/1-1_HelloFW.py
+++ 1_lesson/1-1_HelloFW.py
@@ -1,16 +1,8 @@
 def HelloFW():
 	print "Hello, fucking world!"
 	
-def HelloFWPro(suffix="fucking world") :
-  	print "Hello, " + suffix
-
-def Greet(name) :
-	HelloFWPro(suffix=name)
-
-
-def testMe() :
-	helloFW()
-	helloFW("no need parameter")
-	result = helloFW()
-	print result
+helloFW()
+result = helloFW()
+print result
+helloFW("no need parameter")
 	
