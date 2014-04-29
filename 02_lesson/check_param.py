dd2dc82a793efdb2269d787b20013ca6043a8bbc
Allien Po <besteto@gmail.com>
2014-04-29 17:52:34 +0400

    check param is string

--- 02_lesson/check_param.py
+++ 02_lesson/check_param.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #проверка параметра на тип строки : check(param) -> boolean, возвращает true, если param - строка и false в другом случае
+
+import string
+
+def checkStr(param):
+	isStr = True
+	for everLetter in param:
+		if everLetter not in string.printable:
+			isStr = False
+	return isStr
