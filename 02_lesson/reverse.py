32cce121f8f250be2470b928e9fee19471dce54f
Allien Po <besteto@gmail.com>
2014-04-30 08:19:21 +0400

    reverse string add

--- 02_lesson/reverse.py
+++ 02_lesson/reverse.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #разворот строки : reverse(str) -> str, возвращает строку в обратном порядке
+
+def reverse(param):
+	param = param[::-1]
+	return param
