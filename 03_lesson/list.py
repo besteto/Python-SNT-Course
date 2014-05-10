d7c0e88e11bd807ead575dd7fbf83fb53553a50c
Allien Po <besteto@gmail.com>
2014-05-10 18:28:12 +0400

    first version - dirty, but worked

---
+++ 03_lesson/list.py
@@ -0,0 +1,25 @@
+#charisma, beauty, smart, sexuality
+
+wordAdj = {
+	"Charming" : [6, 7, -2, 3],
+	"Smart" : [-2, 0, 9, 2],
+	"Great" : [4, 4, 4, 4],
+	"Cursed" : [-4, -4, -4, -4]
+}
+#type (1 - armor, 2 - weapon), grade (3 - light, 4 - hard), quality 
+wordNoun = {
+	"Axe" : [2, 4, 15],
+	"Cup" : [1, 3, 3],
+	"Chain Boots" : [1, 4, 20],
+	"Gun" : [2, 4, 55],
+	"Knife" : [2, 3, 7],
+	"Chain" : [2, 3, 8],
+}
+#attribute, quantity
+wordAddNoun = {
+	"of Life" : ["Heal", 9],
+	"by Master" : ["Neutral", 7],
+	"of Chaos" : ["Fire", 22],
+	"of Dragon" : ["Fire", 9],
+	"of Forest" : ["Heal", 3],
+}
---
+++ 03_lesson/main.py
@@ -0,0 +1,10 @@
+from list import *
+import random
+
+def constructName(numb):
+	newName = []
+	for every in range(numb):
+		newName.append(random.choice(list(wordAdj.keys())) + " " + random.choice(list(wordNoun.keys())) + " " + random.choice(list(wordAddNoun.keys())))
+	return newName
+
+print constructName(3)
