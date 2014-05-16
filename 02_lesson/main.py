6beef5778ecdadaa75201765d3886a63043d2416
Allien Po <besteto@gmail.com>
2014-05-16 20:15:44 +0400

    is PyCham worked?

--- 02_lesson/main.py
+++ 02_lesson/main.py
@@ -9,23 +9,23 @@ python main.py "to be, or not to be: that is the question"
 
 import sys
 import re
-from check_param import checkStr 
+from check_param import checkStr
 
 if len(sys.argv) >= 2 and checkStr(sys.argv[1]):
-	ourStr = sys.argv[1]
-	ourStr = ''.join(w[::-1] for w in re.split('(\W+?)',ourStr))
-	print ourStr
+    ourStr = sys.argv[1]
+    ourStr = ''.join(w[::-1] for w in re.split('(\W+?)', ourStr))
+    print ourStr
 
 else:
-	print "invalid commandline"
+    print "invalid commandline"
 
-# Что будет, если позвать программу с русским текстом? Почему? Как исправить?
-# 
-# Оценки : 
-# 3 за реализацию (оно не работает на русских строках)
-# 5 за инициативу - регэксп справляется лучше в данном случае
-# Итоговая 4. Программа решает только поставленную (и бессмысленную) задачу, просто
-# расширить до чего-то полезного - не выйдет.
-#
-#
-#
+    # Что будет, если позвать программу с русским текстом? Почему? Как исправить?
+    #
+    # Оценки :
+    # 3 за реализацию (оно не работает на русских строках)
+    # 5 за инициативу - регэксп справляется лучше в данном случае
+    # Итоговая 4. Программа решает только поставленную (и бессмысленную) задачу, просто
+    # расширить до чего-то полезного - не выйдет.
+    #
+    #
+    #
