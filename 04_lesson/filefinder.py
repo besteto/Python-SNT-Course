edad0d59eb81f07230d53182816c0427563fabfe
Ururu <besteto@gmail.com>
2014-05-28 17:29:44 +0400

    file finder can take several extensions at a time!

--- 04_lesson/filefinder.py
+++ 04_lesson/filefinder.py
@@ -6,16 +6,18 @@ def filefinder():
     parser = argparse.ArgumentParser(description='wanna see list of files')
 
     parser.add_argument("--directory",  type = str, default = "/Users/iUruru/Develop/", help = "where you hope to find")
-    parser.add_argument("--extensions", type = str, default = "*.md", action = "store", help = "one kind of files, like *.py")
+    parser.add_argument("--extensions", type = str, default = "*.md", action = "store", help = "extensions with comma, like *.py, *.txt")
 
     options = parser.parse_args()
+    listExtensions = [word.strip(" ") for word in options.extensions.split(",")]
 
     for root, dirs, files in os.walk(options.directory):
         for nameFile in files:
-            if fnmatch.fnmatch(nameFile, options.extensions):
-                print (os.path.join(root, nameFile))
+            for every in listExtensions:
+                if fnmatch.fnmatch(nameFile, every):
+                    print (os.path.join(root, nameFile))
 
     print("path is " + str(options.directory))
-    print("ext is " + str(options.extensions))
+    print("ext is " + str(listExtensions))
 
 filefinder()
