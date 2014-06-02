0eb88dec115084085233c91f125ddd52e643aac2
Ururu <besteto@gmail.com>
2014-06-03 00:19:39 +0400

    filefinder now is a module and only find files

--- 04_lesson/filefinder.py
+++ 04_lesson/filefinder.py
@@ -1,14 +1,11 @@
 import os
 import fnmatch
-import datetime
 
 def filefinder(dir, ext):
-
-    with open(str(datetime.datetime.now())+"files_list.txt", "w") as listFiles:
-        listFiles.write("path is " + str(dir) + "\n")
-        listFiles.write("ext is " + str(ext) + "\n")
-        for root, dirs, files in os.walk(dir):
-            for nameFile in files:
-                for every in ext:
-                    if fnmatch.fnmatch(nameFile, every):
-                        listFiles.write(os.path.join(root, nameFile) + "\n")
+    filesList = []
+    for root, dirs, files in os.walk(dir):
+        for nameFile in files:
+            for every in ext:
+                if fnmatch.fnmatch(nameFile, every):
+                    filesList.append(os.path.join(root, nameFile))
+    return filesList
--- 04_lesson/main.py
+++ 04_lesson/main.py
@@ -1,12 +1,18 @@
 import argparse
 import filefinder
+import datetime
 
 parser = argparse.ArgumentParser(description='wanna see list of files')
 
 parser.add_argument("--directory",  type = str, default = "/Users/iUruru/Develop/", help = "where you hope to find")
-parser.add_argument("--extensions", type = str, default = "*.md", action = "store", help = "extensions with comma, like *.py, *.txt")
+parser.add_argument("--extensions", type = str, default = "*.md", action = "store", help = "extensions with comma, like '*.py, *.txt'")
 
 options = parser.parse_args()
 listExtensions = [word.strip(" ") for word in options.extensions.split(",")]
 
-filefinder.filefinder(options.directory, listExtensions)
+with open(str(datetime.datetime.now())+"files_list.txt", "w") as listFiles:
+    listFiles.write("path is " + str(options.directory) + "\n")
+    listFiles.write("ext is " + str(options.extensions) + "\n")
+
+    for every in filefinder.filefinder(options.directory, listExtensions):
+        listFiles.write(every + "\n")
